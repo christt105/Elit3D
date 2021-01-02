31bc625525310be7896eb914115b44445430822b
Christian Martínez de la Rosa <36189169+christt105@users.noreply.github.com>
2021-01-02 21:34:10 +0100

    Update selectTile.fragment.glsl

--- src/binaries/Configuration/Shader/Shaders/selectTile.fragment.glsl
+++ src/binaries/Configuration/Shader/Shaders/selectTile.fragment.glsl
@@ -25,98 +25,136 @@ vec4 orange = vec4(0.9, 0.470, 0.0, 1.0);
 vec4 red = vec4(1.0, 0.0, 0.0, 1.0);
 vec4 green = vec4(0.0, 1.0, 0.0, 1.0);
 
-uniform bool loco = true;
-
-void DrawTexturedTile() {
+void DrawTexturedTile()
+{
 	//Calculate the position of the atlas
-			vec2 coord = TexCoord * brushSize;
-			coord = coord - floor(coord);
-			vec2 atlasPos = (coord + tileSelected) / ntilesAtlas;
-			if (locked) {
-				if (tool == 0 || tool == 2) {
-					if (tileSelected.x == -1.0) {
-						discard;
-					}
-					else {
-						FragColor = vec4(texture(TextureAtlas, atlasPos).xyz, 0.75) * red;
-					}
-				}
-				else {
-					FragColor = vec4(red.xyz, 0.2);
-				}
+	vec2 coord = TexCoord * brushSize;
+	coord = coord - floor(coord);
+	vec2 atlasPos = (coord + tileSelected) / ntilesAtlas;
+	if (locked)
+	{
+		if (tool == 0 || tool == 2)
+		{
+			if (tileSelected.x == -1.0)
+			{
+				discard;
 			}
-			else {
-				if (tool == 0 || tool == 2) {
-					if (tileSelected.x == -1.0) {
-						discard;
-					}
-					else {
-						FragColor = vec4(texture(TextureAtlas, atlasPos).xyz, 0.75) /* + vec4(0.9, 0.470, 0.0, 0.2)*/;
-					}
-				}
-				else {
-					FragColor = vec4(orange.xyz, 0.2);
-				}
+			else
+			{
+				FragColor = vec4(texture(TextureAtlas, atlasPos).xyz, 0.75) *red;
 			}
+		}
+		else
+		{
+			FragColor = vec4(red.xyz, 0.2);
+		}
+	}
+	else
+	{
+		if (tool == 0 || tool == 2)
+		{
+			if (tileSelected.x == -1.0)
+			{
+				discard;
+			}
+			else
+			{
+				FragColor = vec4(texture(TextureAtlas, atlasPos).xyz, 0.75) /*+ vec4(0.9, 0.470, 0.0, 0.2)*/ ;
+			}
+		}
+		else
+		{
+			FragColor = vec4(orange.xyz, 0.2);
+		}
+	}
+}
+
+void DrawBorder()
+{
+	if (tool == 0 || tool == 2 || tool == 3)
+	{
+		if (locked)
+		{
+			FragColor = red;
+		}
+		else
+		{
+			FragColor = green;
+		}
+	}
+	else
+	{
+		FragColor = orange;
+	}
+}
+
+void CircularShape()
+{
+	float r = float(brushSize) *0.5;
+	vec2 uv = TexCoord;
+	if (brushSize % 2 == 0)
+	{
+		uv -= 0.5;
+	}
+
+	uv *= float(brushSize);
+
+	vec2 p = floor(uv) + 0.5;
+	if (brushSize % 2 != 0)
+	{
+		p -= 0.5* float(brushSize);
+	}
+
+	if (p.x *p.x + p.y *p.y <= r *r)
+	{
+		DrawTexturedTile();
+	}
+	else
+	{
+		discard;
+	}
 }
 
 void main()
 {
-	if(loco){
-		vec2 mapPos = mousePos;
-		if(mousePos.x < sizeMap.x-1)
-			mapPos.x += 1.0;
-		if(mousePos.y < sizeMap.y-1)
-			mapPos.y += 1.0;
-		vec4 tileOnMouse = texture(TextureMap, mapPos/sizeMap);
-		vec4 tile = texture(TextureMap, TexCoord);
-		if(tileOnMouse == tile){
-			FragColor = vec4(green.xyz, 0.5);
-		}
-		else{
-			FragColor = vec4(orange.xyz, 0.5);
-		}
-	}
-	else{
-	if (brushShape == 0) {
-  		minRangeBorder = minRangeBorder / brushSize;
-		if (TexCoord.x < minRangeBorder || TexCoord.x > 1.0 - minRangeBorder || TexCoord.y < minRangeBorder || TexCoord.y > 1.0 - minRangeBorder) {
-			if (tool == 0 || tool == 2 || tool == 3) {
-				if (locked) {
-					FragColor = red;
-				}
-				else {
-					FragColor = green;
-				}
-    		}
-			else {
-				FragColor = orange;
+	if (tool != 2)
+	{
+		// BRUSH &ERASER &EYE DROPPER
+		if (brushShape == 0)
+		{
+			minRangeBorder = minRangeBorder / brushSize;
+			if (TexCoord.x < minRangeBorder || TexCoord.x > 1.0 - minRangeBorder || TexCoord.y < minRangeBorder || TexCoord.y > 1.0 - minRangeBorder)
+			{
+				DrawBorder();
+			}
+			else
+			{
+				if (tool != 3)
+					DrawTexturedTile();
 			}
 		}
-		else {
-			if(tool != 3)
-				DrawTexturedTile();
+		else
+		{
+			CircularShape();
 		}
 	}
-	else {
-		float r = float(brushSize) * 0.5;
-		vec2 uv = TexCoord;
-		if (brushSize % 2 == 0) {
-			uv -= 0.5;
+	else
+	{
+		//BUCKET
+		vec2 mapPos = mousePos;
+		if (mousePos.x < sizeMap.x - 1)
+			mapPos.x += 1.0;
+		if (mousePos.y < sizeMap.y - 1)
+			mapPos.y += 1.0;
+		vec4 tileOnMouse = texture(TextureMap, mapPos / sizeMap);
+		vec4 tile = texture(TextureMap, TexCoord);
+		if (tileOnMouse == tile)
+		{
+			FragColor = vec4(green.xyz, 0.5);
 		}
-		uv *= float(brushSize);
-
-		vec2 p = floor(uv) + 0.5;
-		if (brushSize % 2 != 0) {
-			p -= 0.5 * float(brushSize);
+		else
+		{
+			FragColor = vec4(orange.xyz, 0.5);
 		}
-
-		if (p.x*p.x+p.y*p.y <= r*r) {
-			DrawTexturedTile();
-		}
-		else {
-			discard;
-		}
-	}
 	}
 }
